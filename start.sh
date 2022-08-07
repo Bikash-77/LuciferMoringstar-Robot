61f9b289f731880f4846e1f88262a2b09ccdd63d
Bikash Maity <110729541+Bikash-77@users.noreply.github.com>
2022-08-08 00:30:15 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /LuciferMoringstar-Robot
 fi
-cd  /LuciferMoringstar-Robot
+cd /LuciferMoringstar-Robot
 pip3 install -U -r requirements.txt
 echo "Starting Lucifer Moringstar Robot...."
 python3 main.py
